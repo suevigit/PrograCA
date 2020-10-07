bb741036fd095901faed2df7a8a75f15cfb54b74
Susan Vasquez <susan1@mesoamericana.edu.gt>
2020-10-07 13:43:22 -0600

    se agrego como borrador y para publicar se agrego el login y logout se agrego boton eliminar publicacion

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('post/<int:pk>/', views.publicacion_detalle, name='publicacion_detalle'),
     path('publicacion/nueva/', views.publicacion_nueva, name='publicacion_nueva'),
     path('publicacion/<int:pk>/editar/', views.publicacion_editar, name='publicacion_editar'),
+    path('publicacion/borrador/', views.publicacion_borrador_lista, name='publicacion_borrador_lista'),
+    path('publicacion/<int:pk>/publicar', views.publicacion_publicar, name='publicacion_publicar'),
+    path('publicacion/<pk>/eliminar/', views.publicacion_eliminar, name='publicacion_eliminar'),
 ]
--- misitio/settings.py
+++ misitio/settings.py
@@ -123,5 +123,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
+LOGIN_REDIRECT_URL = '/'
 
 X_FRAME_OPTIONS='SAMEORIGIN'
