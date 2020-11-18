e9b33a18a8d82dc07a8c6832314592d44bbfd1bc
Susan Vasquez <susan1@mesoamericana.edu.gt>
2020-11-18 17:33:02 -0600

    error resuelto de modelo

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 class Publicacion(models.Model):
     autor = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    titulo = models.CharField(max_length=50)
+    titulo = models.CharField(max_length=100)
     texto = models.TextField()
     fecha_creacion= models.DateTimeField(
               default=timezone.now)
